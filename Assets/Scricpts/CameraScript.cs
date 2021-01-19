0e7a1d2e00d5420bcc7423690edb9d702dee6bf3
kebal7 <69685391+kebal7@users.noreply.github.com>
2021-01-19 16:41:06 +0545

    mnn

---
+++ Assets/Scricpts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public SpriteRenderer backround;
+
+    float orthoSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orthoSize = backround.bounds.size.x * Screen.height /Screen.width * 0.5f;
+
+        Camera.main.orthographicSize = orthoSize;
+    }
+
+
+}
